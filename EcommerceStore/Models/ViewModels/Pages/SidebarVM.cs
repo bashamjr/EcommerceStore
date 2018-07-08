e009d471b390d9985da6d26c2a29432476fe3102
Jonathan.Basham <Jonathan.Basham@USIT2541L>
2018-07-07 18:36:48 -0700

    Half

---
+++ EcommerceStore/Models/ViewModels/Pages/SidebarVM.cs
@@ -0,0 +1,25 @@
+﻿using EcommerceStore.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceStore.Models.ViewModels.Pages
+{
+    public class SidebarVM
+    {
+        public SidebarVM()
+        {
+         
+        }
+        public SidebarVM(SidebarDTO row)
+        {
+            Id = row.Id;
+            Body = row.Body;
+        }
+        public int Id { get; set; }
+        [AllowHtml]
+        public string Body { get; set; }
+    }
+}
