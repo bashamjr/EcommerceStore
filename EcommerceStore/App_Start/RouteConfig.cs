90610c0e2e2cd4d4d9e0f5aad826de2eedbbb019
Jonathan.Basham <Jonathan.Basham@USIT2541L>
2018-07-09 00:02:57 -0700

    Add Front Pages

--- EcommerceStore/App_Start/RouteConfig.cs
+++ EcommerceStore/App_Start/RouteConfig.cs
@@ -13,11 +13,18 @@ namespace EcommerceStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "EcommerceStore.Controllers" });
+
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "EcommerceStore.Controllers" });
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "EcommerceStore.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "EcommerceStore.Controllers" });
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "EcommerceStore.Controllers" });
+
+           // routes.MapRoute(
+           //     name: "Default",
+           //     url: "{controller}/{action}/{id}",
+           //     defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           //  );
         }
     }
 }
---
+++ EcommerceStore/Controllers/ShopController.cs
@@ -0,0 +1,54 @@
+﻿using EcommerceStore.Models.Data;
+using EcommerceStore.Models.ViewModels.Pages;
+using EcommerceStore.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Pages");
+        }
+
+        public ActionResult CategoryMenuPartial()
+        {
+            // Declare list of CategoryVM
+            List<CategoryVM> categoryVMList;
+            // Init the list
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+            // Return partial with list
+            return PartialView(categoryVMList);
+        }
+
+        // GET: /Shop/Category/Name
+        public ActionResult Category(string name)
+        {
+            // Declare a list of ProductVM
+            List<ProductVM> productVMList;
+
+            using (Db db = new Db())
+            {
+                // Get category id
+                CategoryDTO categorydto = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+                int catId = categorydto.Id;
+                // Init the list
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+                // Get category name
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+                ViewBag.CategoryName = productCat.CategoryName;
+            }
+            // Return view with list
+            return View(productVMList);
+        }
+    }
+}
